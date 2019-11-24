c31307109ae7ea9936648f4bb01ac6061b644e12
serpentiem <>
2019-11-24 21:05:30 +0100

    Invert Fast Camera as well.

--- Mary/System/Camera.cpp
+++ Mary/System/Camera.cpp
@@ -5,12 +5,18 @@ void System_Camera_ToggleInvertX(bool enable)
 	Log("%s %u", FUNC_NAME, enable);
 	if (enable)
 	{
-		Write<dword>((appBaseAddr + 0x57726), 0x00002000);
-		Write<dword>((appBaseAddr + 0x57752), 0x00008000);
+		// Default
+		Write<dword>((appBaseAddr + 0x57726), GAMEPAD_RIGHT); // turn camera right
+		Write<dword>((appBaseAddr + 0x57752), GAMEPAD_LEFT ); // turn camera left
+		// Fast Camera
+		Write<dword>((appBaseAddr + 0x51267), GAMEPAD_LEFT ); // turn camera left
+		Write<dword>((appBaseAddr + 0x51296), GAMEPAD_RIGHT); // turn camera right
 	}
 	else
 	{
-		Write<dword>((appBaseAddr + 0x57726), 0x00008000);
-		Write<dword>((appBaseAddr + 0x57752), 0x00002000);
+		Write<dword>((appBaseAddr + 0x57726), GAMEPAD_LEFT );
+		Write<dword>((appBaseAddr + 0x57752), GAMEPAD_RIGHT);
+		Write<dword>((appBaseAddr + 0x51267), GAMEPAD_RIGHT);
+		Write<dword>((appBaseAddr + 0x51296), GAMEPAD_LEFT );
 	}
 }
--- Mary/System/Camera.h
+++ Mary/System/Camera.h
@@ -1,4 +1,5 @@
 #pragma once
 #include "../../Core/Core.h"
+#include "../Vars.h"
 
 void System_Camera_ToggleInvertX(bool enable);
